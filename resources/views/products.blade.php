7ba20af767988503a8b60aa3037643e9d6abb407
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-18 16:55:32 +0300

    first commit

---
+++ resources/views/products.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <main>
+        <article>
+            <h2>Doctor's Sausage</h2>
+            <img src="/images/Doktordesa.png" alt="Big Juicy Sausage" width="200px"/>
+            <p>This is the greatest sausage of all time! Without taking a bite, you will miss the night..</p>
+            <p>1.49 EUR</p>
+        </article>
+    </main>
+</body>
+</html>
